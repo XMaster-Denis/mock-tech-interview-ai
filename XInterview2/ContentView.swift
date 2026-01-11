fa6482eb2e0263728063b8f50b1e312ab1b34b8f
XMaster <xmaster.denis@gmail.com>
2026-01-11 15:39:37 +0100

    Initial Commit

---
+++ XInterview2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XInterview2
+//
+//  Created by XMaster on 11.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XInterview2/XInterview2App.swift
@@ -0,0 +1,17 @@
+//
+//  XInterview2App.swift
+//  XInterview2
+//
+//  Created by XMaster on 11.01.26.
+//
+
+import SwiftUI
+
+@main
+struct XInterview2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
